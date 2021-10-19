33993be528e4f08ad0029717507d5bfc7b143bce
TitasKanti <adv.titas.bd@gmail.com>
2021-10-20 04:35:58 +0600

    detail card update

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const ServiceDetail = () => {
     const { id } = useParams();
     const [details, setDetails] = useState([]);
     const [detail, setDetail] = useState({});
+    const history = useHistory();
+
+    const handleClick = () => {
+        history.push('/home');
+    }
+
     useEffect(() => {
         fetch('/fakeData.json')
             .then(res => res.json())
@@ -17,8 +29,28 @@ const ServiceDetail = () => {
     }, [details])
 
     return (
-        <div>
-            <h2>here is service details {id} {detail?.name} <br /> {detail?.detail}</h2>
+        <div className="flex justify-center m-2">
+            <Card sx={{ maxWidth: 500 }}>
+                <CardMedia
+                    component="img"
+                    alt="green iguana"
+                    height="140"
+                    image={detail?.img}
+                />
+                <CardContent className="text-justify">
+                    <Typography className="text-blue-700" gutterBottom variant="h5" component="div">
+                        {detail?.name}
+                    </Typography>
+                    <Typography variant="body1" color="text.secondary">
+                        {detail?.description}
+                        <br />
+                        {detail?.detail}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button onClick={handleClick} size="small">Go Back</Button>
+                </CardActions>
+            </Card>
         </div>
     );
 };
